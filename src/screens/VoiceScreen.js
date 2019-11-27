6cef8b58da9df6e44eebb81b8edd59a52f9e3079
Timur_Hook <davletshin.talan@gmail.com>
2019-11-27 14:27:36 +0500

    все работает не на старом андроиде

--- src/screens/VoiceScreen.js
+++ src/screens/VoiceScreen.js
@@ -1,59 +1,66 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Voice from 'react-native-voice';
 
-class VoiceScreen extends React.Component{
-    constructor(props) {
-        super();
-        this.state ={
-            results:[]
-        };
-        Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
-    }
-    onSpeechResultsHandler(e){
-        console.log('%c++','background:lime',e);
-        this.setState({
-            results:e.value
-        });
-    }
-    static navigationOptions = {
-        title: 'Voice Page',
+class VoiceScreen extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      results: [],
     };
-    onSpeechStart(){
-        console.log('%c++','background:','here');
-        Voice.start('ru_RU');
-    }
-    onSpeechEnd(){
-        Voice.stop();
-    }
-    render(){
-        return <View>
-            <Text>Voice Screen Demo</Text>
-            <View style={styles.ViewStyle}>
-                <TouchableOpacity onPress={this.onSpeechStart.bind()} style={styles.btnStyle}>
-                    <Text>Start Voice Recording</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={this.onSpeechEnd.bind()}  style={styles.btnStyle}>
-                    <Text>Stop Voice Recording</Text>
-                </TouchableOpacity>
-            </View>
-            {this.state.results.map((text,index)=>{
-            return <Text key={index}>{text}</Text>
-            })}
+    Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
+    console.log('%c++', 'background:voiceAvailable', Voice.isAvailable());
+  }
+  onSpeechResultsHandler(e) {
+    console.log('%c++', 'background:lime', e);
+    this.setState({
+      results: e.value,
+    });
+  }
+  static navigationOptions = {
+    title: 'Voice Page',
+  };
+  onSpeechStart() {
+    console.log('%c++', 'background:', 'here');
+    Voice.start('ru_RU');
+  }
+  onSpeechEnd() {
+    Voice.stop();
+  }
+  render() {
+    return (
+      <View>
+        <Text>Voice Screen Demo</Text>
+        <View style={styles.ViewStyle}>
+          <TouchableOpacity
+            onPress={this.onSpeechStart.bind()}
+            style={styles.btnStyle}>
+            <Text>Start Voice Recognition</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.onSpeechEnd.bind()}
+            style={styles.btnStyle}>
+            <Text>Stop Voice Recognition</Text>
+          </TouchableOpacity>
         </View>
-    }
+        {this.state.results.map((text, index) => {
+          return <Text key={index}>{text}</Text>;
+        })}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    ViewStyle:{
-        flexDirection:'row',
-        justifyContent:'space-around'
-    },
-    btnStyle:{
-        backgroundColor:'lightblue',
-        width:200,
-        height:60
-    }
+  ViewStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  btnStyle: {
+    backgroundColor: 'lightblue',
+    width: 200,
+    height: 60,
+  },
 });
 
 export default VoiceScreen;
